05635b48256127dc51e3fa18ede367878a8adee3
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-09 23:00:27 +0400

    fix: cors errorss

--- backend/server.js
+++ backend/server.js
@@ -12,25 +12,33 @@ import { connectDB } from './lib/db.js';
 import cors from 'cors';
 dotenv.config();
 
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-  })
-);
-
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true, // if you're using cookies / auth
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/notifications', notificationRoutes);
 app.use('/api/v1/connections', connectionRoutes);
-
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true, // if you're using cookies / auth
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
