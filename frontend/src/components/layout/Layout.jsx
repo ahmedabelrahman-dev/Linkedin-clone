d7e46080fb74d6648bcbf9fcd82c6a3311446c07
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-09 23:11:06 +0400

    feat(component): layout and navbar for the home page

--- frontend/src/components/layout/Layout.jsx
+++ frontend/src/components/layout/Layout.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
-import Navbar from './NavBar.jsx';
+import Navbar from './navbar/Navbar.jsx';
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen big-base-100">
+    <div className="min-h-screen bg-base-100">
       <Navbar />
       <main className="max-w-7xl mx-auto px-4 py-6">{children}</main>
     </div>
   );
 };
-
 export default Layout;
