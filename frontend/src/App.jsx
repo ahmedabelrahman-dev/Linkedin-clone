9a2d5e42218fb1587cae5cb932b7952db94db5a2
ahmedabelrahman-dev <rman79340@gmail.com>
2025-12-09 23:03:26 +0400

    build the app.jsx to handle the routes and authenticated user with protect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,54 @@
-import Layout from './components/layout/Layout.jsx';
-import LoginPage from './pages/auth/LoginPage.jsx';
-import SignUpPage from './pages/auth/SignUpPage.jsx';
-import { Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage.jsx';
+import { toast } from 'react-hot-toast';
 import { Toaster } from 'react-hot-toast';
+import { useQuery } from '@tanstack/react-query';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import { axiosInstance } from './lib/axios';
+
+import HomePage from './pages/HomePage.jsx';
+import LoginPage from './pages/auth/LoginPage.jsx';
+import Layout from './components/layout/Layout.jsx';
+import SignUpPage from './pages/auth/SignUpPage.jsx';
+import NotificationsPage from './pages/NotificationsPage.jsx';
 
 function App() {
+  const { data: authUser, isLoading } = useQuery({
+    queryKey: ['authUser'],
+    queryFn: async () => {
+      try {
+        const res = await axiosInstance.get('/auth/me');
+        return res.data;
+      } catch (err) {
+        if (err.response && err.response.status === 401) {
+          return null;
+        }
+        toast.error(err.response.data.message || 'Something went wrong');
+      }
+    },
+  });
+
+  if (isLoading) return null;
   return (
     <Layout>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
+        <Route
+          path="/"
+          element={authUser ? <HomePage /> : <Navigate to={'/login'} />}
+        />
+        <Route
+          path="/signup"
+          element={!authUser ? <SignUpPage /> : <Navigate to={'/'} />}
+        />
+        <Route
+          path="/login"
+          element={!authUser ? <LoginPage /> : <Navigate to={'/'} />}
+        />
+        <Route
+          path="/notifications"
+          element={
+            authUser ? <NotificationsPage /> : <Navigate to={'/login'} />
+          }
+        />
       </Routes>
       <Toaster />
     </Layout>
